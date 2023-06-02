95a8cfbd345e653e961f991b3b2267f79ae8191a
rakshith299 <133012343+rakshith299@users.noreply.github.com>
2023-06-02 17:14:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+var ans = confirm("Do you want to proceed?");
+
+if(ans == true){
+	alert(`You clicked OK. Proceeding...`);
+}else{
+	alert(`You clicked Cancel. Exiting...`);
+}
